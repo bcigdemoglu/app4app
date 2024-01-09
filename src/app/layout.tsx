48aa0d136ac4546527eaddd286d9408a4fb33ea1
bcigdemoglu <mbugrahanc+github@gmail.com>
2024-01-09 15:08:50 +0300

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import Hotjar from './components/Hotjar';
 import GoogleAnalytics from './components/GoogleAnalytics';
+import { Analytics as VercelAnalytics } from '@vercel/analytics/react';
 
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css';
@@ -32,7 +33,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <VercelAnalytics />
+      </body>
       <Hotjar />
       <GoogleAnalytics />
     </html>
