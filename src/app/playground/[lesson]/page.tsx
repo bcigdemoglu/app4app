c082c2d35d247aa21b6b8e5d02053279fe87c04a
bcigdemoglu <mbugrahanc+github@gmail.com>
2024-01-15 13:42:30 +0300

    Working UI

--- src/app/playground/[lesson]/page.tsx
+++ src/app/playground/[lesson]/page.tsx
@@ -1,8 +1,9 @@
-import LessonSections from '@/app/components/lesson';
-import { LessonMap, getLessonMDX } from '@/app/components/lesson/data';
+import LessonSections from '@/app/components/LessonSection';
+import { LessonMap, getLessonMDX } from '@/app/components/LessonSection/data';
 import { notFound } from 'next/navigation';
 import { getRecordMap } from '@/app/actions';
 import NotionPage from '@/app/components/NotionPage/NotionPage';
+import Image from 'next/image';
 
 export const metadata = {
   title: "Ilayda's Playground: How to Start a Business",
@@ -32,14 +33,23 @@ export default async function Page({ params }: Props) {
   const { mdxInputSource, mdxOutputSource } = await getLessonMDX(recordMap);
 
   return (
-    <main className='grid h-screen grid-cols-3 bg-sky-50'>
+    <main className='grid h-screen grid-cols-3 gap-1 bg-sky-100'>
       <header className='col-span-3 grid grid-cols-2 p-2'>
         <div className='flex justify-start gap-2'>
-          <span>{'ilayda.com'}</span>
+          <span className='transform rounded-lg bg-gradient-to-r from-blue-400 to-purple-600 px-4 py-1 text-xl font-bold text-white shadow-md transition-transform duration-300 ease-in-out hover:from-purple-600 hover:to-blue-400'>
+            {"ilayda's playground"}
+          </span>
         </div>
-        <div className='flex justify-end gap-2'>
-          <button className=' bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 disabled:bg-blue-300'>
-            My account
+        <div className='flex justify-end gap-1'>
+          <button className=' '>
+            <Image
+              src='/ilayda.jpeg'
+              alt='Ilayda Buyukdogan Profile'
+              className='h-10 w-10 overflow-hidden rounded-full border object-cover'
+              width={100}
+              height={0}
+              sizes='100vw'
+            />
           </button>
           <button className='rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700 disabled:bg-blue-300'>
             My account
