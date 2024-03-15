19c29f08a1194edb5eef54ad7abdb1fb96fa3d8b
bcigdemoglu <mbugrahanc+github@gmail.com>
2024-03-15 19:42:28 +0300

    Better login

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -6,18 +6,18 @@ export default function UserLayout({
   return (
     <section>
       <div className='bg-secondary-50 flex min-h-screen overflow-y-auto'>
-        <div className='flex w-full items-center justify-center lg:w-1/2'>
+        <div className='flex w-full items-center justify-center md:w-1/2'>
           {/* Sign-up Form Section */}
           <div className='mx-auto w-full max-w-md space-y-6'>{children}</div>
         </div>
         <div className='hidden items-center justify-center bg-gradient-to-r from-blue-600 to-blue-800 lg:flex lg:w-1/2'>
           {/* Illustration*/}
-          <div className='flex max-w-lg flex-col items-center space-y-8 pb-24'>
+          <div className='flex flex-col items-center space-y-8'>
             <h1 className='text-center text-5xl font-thin tracking-[-0.025em] text-white'>
-              Get Your Idea ✍🏻<br></br>Ready for the Real World 🎯
+              Get Ready for Your Interactive Experience
             </h1>
             <h3 className='text-2xl font-black tracking-[-0.02em] text-white'>
-              ilayda.com
+              cloudybook.com
             </h3>
           </div>
         </div>
