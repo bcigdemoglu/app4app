0196f2854a19b957e0323a61acf1337779cd74f5
bcigdemoglu <mbugrahanc+github@gmail.com>
2024-04-15 15:35:36 +0300

    Limit middleware to playground paths

--- src/middleware.ts
+++ src/middleware.ts
@@ -78,6 +78,6 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      */
-    '/((?!api|_next/static|_next/image|favicon.ico).*)',
+    '/playground/:path*',
   ],
 };
